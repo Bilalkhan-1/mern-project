859e61748fb32c7b14160d5c8b29a966225d0a92
bilalkhan-1 <bilal.habib@devsinc.com>
2022-04-12 15:59:14 +0500

    backend folder structure

--- server/index.js
+++ server/index.js
@@ -3,7 +3,11 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import postRoutes from "./routes/posts.js";
+
 const app = express();
+
+app.use("/posts", postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -22,5 +26,3 @@ mongoose
     app.listen(PORT, () => console.log("server running on port: ", PORT))
   )
   .catch((error) => console.log(error.message));
-
-// mongoose.set("useFindAndModify", false);
